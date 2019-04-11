99c44f4edf87529ecc832575810e97ab9ebf783c
Kazuhira893 <46933930+Kazuhira893@users.noreply.github.com>
2019-04-11 09:55:43 -0400

    update

--- js_practice/js/main.js
+++ js_practice/js/main.js
@@ -1,47 +1,47 @@
-// JavaScript Document
+ //JavaScript Document
 $(document).ready(function()
 {
 
 $('#title5').click(function()
       {
-        $('#song5').toggleClass('open');
+        $('#song5').toggleClass('expand');
       });
     $('#yo').click(function()
       {
-        $('#song4').toggleClass('open');
+        $('#song4').toggleClass('expand');
       });
     $('#title3').click(function()
       {
-        $('#song3').toggleClass('open');
+        $('#song3').toggleClass('expand');
       });
     $('#title2').click(function()
       {
-        $('#song2').toggleClass('open');
+        $('#song2').toggleClass('expand');
       });
     $('#title1').click(function()
       {
-        $('#song1').toggleClass('open');
+        $('#song1').toggleClass('epxand');
       });
-
+$('#song1').click(function()
+      {
+        $('#song1').toggleClass('expand');
+      });
     $('#song5').click(function()
       {
-        $('#title5').toggleClass('open');
+        $('#title5').toggleClass('expand');
       });
     $('#song4').click(function()
       {
-        $('#song4').toggleClass('open');
+        $('#song4').toggleClass('expand');
       });
     $('#song3').click(function()
       {
-        $('#song3').toggleClass('open');
+        $('#song3').toggleClass('expand');
       });
     $('#song2').click(function()
       {
-        $('#song2').toggleClass('open');
+        $('#song2').toggleClass('expand');
       });
-    $('#song1').click(function()
-      {
-        $('#song1').toggleClass('open');
-      });
+   
   });
 

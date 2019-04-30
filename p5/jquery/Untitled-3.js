1f3769a91ad1ba27d98c45043593ae183fab3449
Kazuhira893 <46933930+Kazuhira893@users.noreply.github.com>
2019-04-30 11:15:26 -0400

    update

--- p5/jquery/Untitled-3.js
+++ p5/jquery/Untitled-3.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
 	
 	var images = [
 		
-		"img/687474703a2f2f7075752e73682f344d47796c2e504e47[1].png"
-		"img/3099779fpbfner6zj[2].gif""
-		"img/SCARY[1].jpg"
-		"img/gu_announcement_01[1].png"
+		"img/687474703a2f2f7075752e73682f344d47796c2e504e47[1].png",
+		"img/3099779fpbfner6zj[2].gif",
+		"img/SCARY[1].jpg",
+		"img/gu_announcement_01[1].png",
 		
 	];
 	
 	function randImg(){
-		var size = images.length
-		var x =
+		var size = images.length;
+		var x = 0;
 		
 	}
 	

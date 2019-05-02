a23757ed2fb9effd63e9ace7d7bb0f7bcee3988c
Kazuhira893 <46933930+Kazuhira893@users.noreply.github.com>
2019-05-02 12:39:59 -0400

    update

--- p5/jquery/Untitled-3.js
+++ p5/jquery/Untitled-3.js
@@ -1,19 +1,14 @@
 // JavaScript Document
 $(document).ready(function(){
 	
-	var images = [
+	$('#text').click(function(){
 		
-		"img/687474703a2f2f7075752e73682f344d47796c2e504e47[1].png",
-		"img/3099779fpbfner6zj[2].gif",
-		"img/SCARY[1].jpg",
-		"img/gu_announcement_01[1].png",
-		
-	];
+	$('#text').text('ARE YOU SATISFIED YET?');
 	
-	function randImg(){
-		var size = images.length;
-		var x = 0;
-		
-	}
+	$('.image').css('visibility', "visible");
+		$('#audio').html('autoplay', "true");
 	
-});
+	})
+	
+	
+})
